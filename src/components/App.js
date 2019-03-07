cd92e32cdebe5e4f2e04a0ab9ab9fbdab2932245
Ilya Gershman <ilya@sogosystems.com>
2019-03-07 15:57:49 +0200

    first

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return <ComponentsGenerator />;
+};
+
+class ComponentsGenerator extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+	state = {
+		numChildren: 0,
+		parent: ''
+    };
+
+    onAddChild = (e) => {
+		this.setState({
+			numChildren: this.state.numChildren + 1
+        });
+        
+	};
+
+	render() {
+        const children = [];
+
+        for (var i = 0; i < this.state.numChildren; i += 1) {
+            children.push(<ComponentsGenerator key={i} number={i} />);
+        }
+
+		return (
+            <div className="card" onClick={((e) => { e.stopPropagation(); this.onAddChild(e) })} >{children}</div>
+		);
+	}
+}
+  
+export default App;
---
+++ src/components/ComponentsGenerator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class ComponentsGenerator extends Component {
+    constructor(props) {
+        super(props);
+
+        
+    }
+
+	render() {
+		return <div />;
+	}
+}
+
+export default ComponentsGenerator;
